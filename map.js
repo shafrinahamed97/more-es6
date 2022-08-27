c75ec897b6ab38b60e54e297666419d63606ba9c
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-27 11:59:02 +0600

    create Array and using 2 function

--- map.js
+++ map.js
@@ -7,11 +7,16 @@ function getDoubles(numbers)
     const output = [];
     for(const number of numbers)
     {
-        const doubled = number*2;
+        const doubled = doubleIt(number);
         output.push(doubled);
     }
     return output
 }
+
+function doubleIt(number)
+{
+   return number * 2;
+}
 const result = getDoubles(numbers);
-console.log(result)
+console.log(result);
 
