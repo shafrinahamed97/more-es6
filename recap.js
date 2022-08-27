78a9384d0814af8bfcacd14c3b7570aa889be4dd
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-27 10:43:21 +0600

    Module Introduction and Basic ES6 Recap

--- recap.js
+++ recap.js
@@ -6,9 +6,31 @@ let salary = 450;
 salary = 455;
 
 // 2. default parameters
-function calculateSalary(salary, tax, bonus)
+function calculateSalary(salary, tax=0.25, bonus = 0)
 {
    const remaining = salary - salary*tax;
    const total = remaining + bonus;
    return total
 }
+
+// 3. template string
+const elementHTML = `
+<div>
+    <h3>NameL </h3>
+    <p>Address: </p>
+    <p>Salary:${calculateSalary(10000,0,0)}  </p>
+    <p>Others: ${numbers[2]} </p>
+</div>
+`
+
+// 4. Arrow funtion
+const doubleIt = x => x*2;
+const calculateSalary2 = (salary,tax,bonus)=> salary-salary*tax + bonus;
+
+// 5. spread
+const ages = [11,13,15,14,10,16];
+const newAges = [...ages,22, 24,23];
+
+// 6. dedstructuting
+const{x,y,z, ...c} = {x: 45 , y:12,z:33, name:'shakib Al Hasan', salary:45000}
+const [a,b,...r] = [12,45,24,45,65,98];
