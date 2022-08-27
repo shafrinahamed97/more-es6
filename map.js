5490502f8fbe083fb4c0e9976855eb8109256bd1
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-08-27 12:22:52 +0600

    Map string array, array of objects map, foreach

--- map.js
+++ map.js
@@ -13,10 +13,23 @@ function getDoubles(numbers)
     return output
 }
 
-function doubleIt(number)
+function doubleItold(number)
 {
    return number * 2;
 }
-const result = getDoubles(numbers);
-console.log(result);
+
+const doubleIt = num => num*2;
+const makeDoubleDirect = numbers.map(num => num*3);
+
+console.log(makeDoubleDirect);
+
+/*
+   1. get an array
+   2. for every elements of the of the array do something
+   3. store the result in an array
+   4. return the result array
+*/
+// const result = getDoubles(numbers);
+// console.log(result);
+// console.log(makeDouble);
 
